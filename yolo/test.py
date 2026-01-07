72643aeb163aa74f1da8a0ad54175233944bfae4
dunescifye <dunescifye@gmail.com>
2026-01-07 14:15:06 -0500

    changes

--- yolo/test.py
+++ yolo/test.py
@@ -140,9 +140,9 @@ try:
             # MOVE TOWARDS CENTER
 
             raven_board.set_motor_torque_factor(Raven.MotorChannel.CH3, 50)
-            raven_board.set_motor_speed_factor(Raven.MotorChannel.CH3, 10, reverse=True)
+            raven_board.set_motor_speed_factor(Raven.MotorChannel.CH3, 30, reverse=False)
             raven_board.set_motor_torque_factor(Raven.MotorChannel.CH2, 50)
-            raven_board.set_motor_speed_factor(Raven.MotorChannel.CH2, 10, reverse=False)
+            raven_board.set_motor_speed_factor(Raven.MotorChannel.CH2, 30, reverse=True)
             changed = True
             print("Found object " + classname)
             break
