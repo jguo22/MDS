4d3de16748fb6ee0a6e118e302726f757efc52e0
dunescifye <dunescifye@gmail.com>
2026-01-07 14:11:02 -0500

    changes

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ from raven import Raven
 
 raven_board = Raven()
 
-raven_board.set_motor_encoder(Raven.MotorChannel.CH1, 0) # Set encoder count for motor 1 to zero
-print(raven_board.get_motor_encoder(Raven.MotorChannel.CH1)) # Print encoder count = "0"
+raven_board.set_motor_encoder(Raven.MotorChannel.CH2, 0) # Set encoder count for motor 1 to zero
+print(raven_board.get_motor_encoder(Raven.MotorChannel.CH2)) # Print encoder count = "0"
 
-raven_board.set_motor_mode(Raven.MotorChannel.CH1, Raven.MotorMode.DIRECT) # Set motor mode to DIRECT
+raven_board.set_motor_mode(Raven.MotorChannel.CH2, Raven.MotorMode.DIRECT) # Set motor mode to DIRECT
 
 # Speed controlled:
-raven_board.set_motor_torque_factor(Raven.MotorChannel.CH1, 100) # Let the motor use all the torque to get to speed factor
-raven_board.set_motor_speed_factor(Raven.MotorChannel.CH1, 10, reverse=True) # Spin at 10% max speed in reverse
+raven_board.set_motor_torque_factor(Raven.MotorChannel.CH2, 100) # Let the motor use all the torque to get to speed factor
+raven_board.set_motor_speed_factor(Raven.MotorChannel.CH2, 10, reverse=True) # Spin at 10% max speed in reverse
 
 # Torque controlled:
-# raven_board.set_motor_speed_factor(Raven.MotorChannel.CH1, 100) # Make motor try to run at max speed forward
-# raven_board.set_motor_torque_factor(Raven.MotorChannel.CH1, 100) # Let it use up to 10% available torque
+# raven_board.set_motor_speed_factor(Raven.MotorChannel.CH2, 100) # Make motor try to run at max speed forward
+# raven_board.set_motor_torque_factor(Raven.MotorChannel.CH2, 100) # Let it use up to 10% available torque
 while True:
     pass
 
