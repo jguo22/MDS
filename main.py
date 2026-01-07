95365b905b00425951e3d6c8d959a6dfb5372a63
dunescifye <dunescifye@gmail.com>
2026-01-07 14:10:13 -0500

    changes

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ raven_board.set_motor_mode(Raven.MotorChannel.CH1, Raven.MotorMode.DIRECT) # Set
 
 # Speed controlled:
 raven_board.set_motor_torque_factor(Raven.MotorChannel.CH1, 100) # Let the motor use all the torque to get to speed factor
-raven_board.set_motor_speed_factor(Raven.MotorChannel.CH1, 100, reverse=True) # Spin at 10% max speed in reverse
+raven_board.set_motor_speed_factor(Raven.MotorChannel.CH1, 10, reverse=True) # Spin at 10% max speed in reverse
 
 # Torque controlled:
 # raven_board.set_motor_speed_factor(Raven.MotorChannel.CH1, 100) # Make motor try to run at max speed forward
