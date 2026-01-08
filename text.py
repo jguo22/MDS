74d2f51fdda72bb474ab79d68180f1f29511cd3f
dunescifye <dunescifye@gmail.com>
2026-01-08 18:13:08 -0500

    changes

--- text.py
+++ text.py
@@ -35,7 +35,7 @@ for motor in [LEFT_MOTOR, RIGHT_MOTOR]:
     raven.set_motor_target(motor, 0)
 
 raven.set_motor_pid(RIGHT_MOTOR, p_gain = 25, i_gain = 20, d_gain = 0.3)
-raven.set_motor_pid(LEFT_MOTOR, p_gain = 30, i_gain = 20, d_gain = 0.35)
+raven.set_motor_pid(LEFT_MOTOR, p_gain = 45, i_gain = 20, d_gain = 0.5)
 
 def startPath(left_coefficient, right_coefficient, distance):
     global total_distance, left_coef, right_coef, start_angle, start_left, start_right
