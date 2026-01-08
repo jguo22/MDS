adfa490970e202eebc23578865e63671166a1593
dunescifye <dunescifye@gmail.com>
2026-01-08 17:45:54 -0500

    changes

--- text.py
+++ text.py
@@ -32,7 +32,7 @@ for motor in [LEFT_MOTOR, RIGHT_MOTOR]:
     raven.set_motor_encoder(motor, 0)
     raven.set_motor_max_current(motor, 5)
     raven.set_motor_mode(motor, Raven.MotorMode.POSITION)
-    raven.set_motor_pid(motor, p_gain = 30, i_gain = 0, d_gain = .2)
+    raven.set_motor_pid(motor, p_gain = 20, i_gain = 0, d_gain = .2)
     raven.set_motor_target(motor, 0)
 
 def startPath(left_coefficient, right_coefficient, distance):
