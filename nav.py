7de98dea6d6b5f6abcfce6a0df1c24cd2e60c0b1
jguo22 <junhui@mit.edu>
2026-01-14 16:17:43 -0500

    changes to nav

--- nav.py
+++ nav.py
@@ -51,8 +51,11 @@ class Nav:
             print(self.bno.quaternion)
             time.sleep(0.02)
 
-        self.max_velocity = 10.0 * TICK_ROTATION  # ticks/s
-        self.acceleration = 2.0 * TICK_ROTATION  # ticks/s^2. Reach max v in 1s
+        self._updateAngle()
+        self.start_angle = self.angle
+
+        self.max_velocity = 3.0 * TICK_ROTATION  # ticks/s
+        self.acceleration = 5.0 * TICK_ROTATION  # ticks/s^2. Reach max v in 1s
 
         self.moves: list[NavMove] = []
         self._lock = threading.Lock()
