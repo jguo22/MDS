030b368ea2e469440aa10decc79f45902c32167a
team2pi <junhui@mit.edu>
2026-01-15 18:55:22 -0500

    changes

---
+++ otest.py
@@ -0,0 +1,10 @@
+from raven import Raven
+import time
+
+raven = Raven()
+
+while True:
+	print(raven.get_odometry())
+	print(raven.get_motor_encoder(motor_channel=Raven.MotorChannel.CH2))
+	raven.set_angle(0)
+	time.sleep(0.001)
