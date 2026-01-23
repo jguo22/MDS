7d5cccaf77f7b8475a6842cbb8d510d2cb8df144
jguo22 <junhui@mit.edu>
2026-01-23 11:38:06 -0500

    stuff

--- connection/config.py
+++ connection/config.py
@@ -11,8 +11,8 @@ VIDEO_PORT = 5000  # Port for video streaming
 COMMAND_PORT = 5001  # Port for command data
 
 # Video settings
-FRAME_WIDTH = 640
-FRAME_HEIGHT = 480
+FRAME_WIDTH = 800
+FRAME_HEIGHT = 450
 JPEG_QUALITY = 80  # 0-100, higher = better quality but more bandwidth
 DEFAULT_MAX_FPS = 30.0  # Default maximum frames per second for streaming
 
